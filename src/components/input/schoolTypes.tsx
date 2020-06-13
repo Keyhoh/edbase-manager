b3d9801928092a3b8a812b040374fef5fb29f439
keyhoh <iheyhoh+git@gmail.com>
2020-06-13 22:53:42 +0900

    update: 複数選択項目をjsonから取得

--- src/components/input/schoolTypes.tsx
+++ src/components/input/schoolTypes.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
-import { Form, Row, Col } from "react-bootstrap"
+import { Col, Form, Row } from "react-bootstrap"
 import { graphql, useStaticQuery } from "gatsby"
+import { v4 as uuid } from "uuid"
 
 /**
  * 実践の現場となっている学校種
@@ -17,21 +18,20 @@ const SchoolTypes = () => {
       dataJson {
         school_types {
           name
+          format
         }
       }
     }`
   )
+  const getChecks = (checkList: string[]) => {
+    return checkList.map((name: string) => (
+      <Form.Check key={uuid().toString()} name="school_types" className="mr-4" label={name} value={name} />
+    ))
+  }
   return (
     <Form.Group as={Row} controlId="school-types">
       <Form.Label column sm="2">{school_types.name}</Form.Label>
-      <Col sm="10" className="d-flex flex-wrap">
-        <Form.Check name="school_types" className="mr-4" id="school-type-kindergarten" label="幼稚園" value="幼稚園"/>
-        <Form.Check name="school_types" className="mr-4" id="school-type-elementary" label="小学校" value="小学校"/>
-        <Form.Check name="school_types" className="mr-4" id="school-type-junior" label="中学校" value="中学校"/>
-        <Form.Check name="school_types" className="mr-4" id="school-type-high" label="高等学校" value="高等学校"/>
-        <Form.Check name="school_types" className="mr-4" id="school-type-special" label="特別支援学校" value="特別支援学校"/>
-        <Form.Check name="school_types" className="mr-4" id="school-type-else" label="その他" value="その他"/>
-      </Col>
+      <Col sm="10" className="d-flex flex-wrap">{getChecks(school_types.format)}</Col>
     </Form.Group>
   )
 }
